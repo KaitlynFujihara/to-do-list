e32bb3e2616d895d3ea8f74e991b5163c0bbd306
Katie Fujihara and Sara Hamilton <katie@>
2018-01-22 13:39:32 -0800

    ability to create new task

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,14 @@ function Task (name, category, dueDate, daysUntil, completed) {
   this.name = name;
   this.category = category;
   this.dueDate = dueDate;
+  this.daysUntil = calculateDaysUntil(dueDate);
   this.completed = false;
 }
 
-// Task.prototype.calculateDaysUntil = function() {
-//   return Task.dueDate - Date.now()
-// }
-
 var calculateDaysUntil = function(dueDate) {
-  return Math.ceil((Date.parse(dueDate) - Date.now())/(1000*3600*24));
+  return Math.ceil((Date.parse(dueDate) - Date.now()) / (1000 * 3600 * 24));
 }
+
 
 // user interface logic
 $(document).ready(function() {
@@ -22,13 +20,10 @@ $(document).ready(function() {
     var inputtedName = $("input#newName").val();
     var inputtedCategory = $("select#newCategory").val();
     var inputtedDueDate = $("input#newDueDate").val();
-
-
-    var newTask = new Task(inputtedName, inputtedCategory, inputtedDueDate);
     var calculatedDaysUntil = calculateDaysUntil(inputtedDueDate);
 
-    console.log(calculatedDaysUntil)
-    console.log(newTask)
+    var newTask = new Task(inputtedName, inputtedCategory, inputtedDueDate, calculateDaysUntil);
+  
     $("input#newName").val("");
     $("select#newCategory").val("");
     $("input#newDueDate").val("");
